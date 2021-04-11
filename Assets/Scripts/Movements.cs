bdac238bfd075819472f4e840b100f4853d3470a
Arista213 <79443757+Arista213@users.noreply.github.com>
2021-04-11 19:41:29 +0500

    A

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -8,19 +8,19 @@ namespace Assets.Scripts
         [SerializeField] private KeyCode KeyS = KeyCode.S;
         [SerializeField] private KeyCode KeyA = KeyCode.A;
         [SerializeField] private KeyCode KeyD = KeyCode.D;
-        [SerializeField] private Vector2 moveY = new Vector2(0, 0.1f);
-        [SerializeField] private Vector2 moveX = new Vector2(0.1f, 0);
+        [SerializeField] readonly Vector2 _moveY = new (0, 0.1f);
+        [SerializeField] readonly Vector2 _moveX = new (0.1f, 0);
 
         private void FixedUpdate()
         {
             if (Input.GetKey(KeyW))
-                GetComponent<Rigidbody2D>().velocity += moveY;
+                GetComponent<Rigidbody2D>().velocity += _moveY;
             if (Input.GetKey(KeyS))
-                GetComponent<Rigidbody2D>().velocity -= moveY;
+                GetComponent<Rigidbody2D>().velocity -= _moveY;
             if (Input.GetKey(KeyA))
-                GetComponent<Rigidbody2D>().velocity -= moveX;
+                GetComponent<Rigidbody2D>().velocity -= _moveX;
             if (Input.GetKey(KeyD))
-                GetComponent<Rigidbody2D>().velocity += moveX;
+                GetComponent<Rigidbody2D>().velocity += _moveX;
         }
     }
 }
