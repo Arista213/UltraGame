bc98c6d3dc989eca386bbcd2fb9368f8e69672b6
Arista213 <79443757+Arista213@users.noreply.github.com>
2021-04-13 19:08:11 +0500

    Added camera controller

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -6,12 +6,26 @@ namespace Assets.Scripts
     public class Movements : MonoBehaviour
     {
         [SerializeField] private float _mSpeed = 5f;
+        private bool isFacingRight = true;
 
         private void FixedUpdate()
         {
             float moveX = Input.GetAxis("Horizontal");
             float moveY = Input.GetAxis("Vertical");
             GetComponent<Rigidbody2D>().velocity = new Vector2(_mSpeed * moveX, _mSpeed * moveY);
+
+            if (moveX > 0 && !isFacingRight)
+                Flip();
+            else if (moveX < 0 && isFacingRight)
+                Flip();
+        }
+
+        private void Flip()
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
         }
     }
 }
