e4ff66fbb36109e380c6bf44ac93bf1bbcfe69e8
Arista213 <79443757+Arista213@users.noreply.github.com>
2021-04-19 13:16:36 +0500

    Revert "Merge pull request #5 from Arista213/Slava"
    This reverts commit fcb04528ca1cd66822b85f9e66dd5cafeb0b4601, reversing
    changes made to 31231c01a1b1841d3389f310c1f9dfb6d372d068.

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -1,26 +1,45 @@
+using System;
 using UnityEngine;
+using UnityEngine.Animations;
 
 namespace Assets.Scripts
 {
-    public class SquareScript : MonoBehaviour
+    public class Movements : MonoBehaviour
     {
-        [SerializeField] private KeyCode KeyW = KeyCode.W;
-        [SerializeField] private KeyCode KeyS = KeyCode.S;
-        [SerializeField] private KeyCode KeyA = KeyCode.A;
-        [SerializeField] private KeyCode KeyD = KeyCode.D;
-        [SerializeField] private Vector2 moveY = new Vector2(0, 0.2f);
-        [SerializeField] private Vector2 moveX = new Vector2(0.2f, 0);
+        [SerializeField] private float _mMaxSpeed = 3f;
+        private bool isFacingRight = true;
+        private Animator anim;
+
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
 
         private void FixedUpdate()
         {
-            if (Input.GetKey(KeyW))
-                GetComponent<Rigidbody2D>().velocity += moveY;
-            if (Input.GetKey(KeyS))
-                GetComponent<Rigidbody2D>().velocity -= moveY;
-            if (Input.GetKey(KeyA))
-                GetComponent<Rigidbody2D>().velocity -= moveX;
-            if (Input.GetKey(KeyD))
-                GetComponent<Rigidbody2D>().velocity += moveX;
+            float moveX = Input.GetAxis("Horizontal");
+            float moveY = Input.GetAxis("Vertical");
+
+            var speed = Math.Max(Mathf.Abs(moveX), Mathf.Abs(moveY));
+            anim.SetFloat("Speed", speed);
+
+            //обращаемся к компоненту персонажа RigidBody2D. задаем ему скорость по оси Х, 
+            //равную значению оси Х умноженное на значение макс. скорости
+            GetComponent<Rigidbody2D>().velocity =
+                new Vector2(moveX * _mMaxSpeed, moveY * _mMaxSpeed);
+
+            if (moveX > 0 && !isFacingRight)
+                Flip();
+            else if (moveX < 0 && isFacingRight)
+                Flip();
+        }
+
+        private void Flip()
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
         }
     }
 }
