73fc399b6cafda21e4c4cbb8e093ea75d157e7fc
q1ne <poiut_2001@mail.ru>
2021-05-24 01:33:06 +0500

    add sometihgn

---
+++ Assets/Scripts/General/MoneyShow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace General
+{
+    
+
+    public class MoneyShow : MonoBehaviour
+    {
+        /*private int money = Resource.Money;
+
+        Text txt;
+        // Start is called before the first frame update
+        void Start()
+        {
+            txt.text = money.ToString();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            txt.text = money.ToString();
+        }*/
+    }
+}
